4c1a81f330bcf8549588766fcddf8ac6a8d03198
soonmo <smkwon97@gmail.com>
2022-08-28 20:46:54 +0900

    MemberController

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // 컨트롤러 어노테이션이 있으면 스프링이 시작될 때 객체화 되어 컨테이너에서 관리된다.
             // @Component 어노테이션이 있으면 스프링 빈으로 자동 등록된다. (스프링 빈으로 등록되어 사용하면 이점이 많다.)
@@ -23,5 +29,29 @@ public class MemberController {
         @Autowired  // 컨테이너에서 MemberService 가져옴.
         public MemberController(MemberService memberService) {
                 this.memberService = memberService;
+        }
+
+        @GetMapping("/members/new")
+        public String createForm() {
+                return "members/createMemberForm";
+        }
+
+        @PostMapping("/members/new")    // members/createMemberForm.html에서 form으로 감싼 것을 post로 받음
+                                          // 데이터를 전달 및 등록할 때는 form을 사용한 post 방식,
+                                          // 데이터를 조회할 때는 url에 바로 입력하는 get 방식을 주로 쓴다.
+        public String create(Member form) {
+                Member member = new Member();
+                member.setName(form.getName());
+
+                memberService.join(member);
+
+                return "redirect:/";
+        }
+
+        @GetMapping("/members")
+        public String list(Model model) {
+                List<Member> members = memberService.findMembers();
+                model.addAttribute("members", members);
+                return "members/memberList";
         }
 }
